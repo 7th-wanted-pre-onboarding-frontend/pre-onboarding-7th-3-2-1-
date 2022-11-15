032bf009250db591b75bf0b4fdc16a6064573bdd
taesung1993 <cheonyulin@gmail.com>
2022-11-16 01:35:08 +0900

    feat: 로그인 페이지 퍼블리싱

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,35 +1,17 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { useRouter } from 'next/router';
-import { useMutation } from '@tanstack/react-query';
-import AuthService from '../utils/services/Auth.service';
 import { useAppDispatch, useAppSelector } from '../utils/store';
 import { setUser } from '../utils/store/userReducer';
 import LocalService from '../utils/services/Local.service';
 import { useEffect } from 'react';
 import CookieService from '../utils/services/Cookie.service';
 
+import { Container } from '@mui/material';
+import LoginForm from '../components/LoginForm';
+
 export default function Login() {
-  const router = useRouter();
   const dispatch = useAppDispatch();
   const state = useAppSelector((state) => state.user);
-  const login = useMutation(AuthService.signIn, {
-    onSuccess: (response) => {
-      const {
-        data: { user }
-      } = response;
-
-      LocalService.set('userId', user.id);
-      dispatch(setUser(user));
-
-      AuthService.autoSignOut(router);
-      router.push('/');
-    }
-  });
-
-  const onLogin = async () => {
-    login.mutate();
-  };
 
   useEffect(() => {
     if (state) {
@@ -49,10 +31,16 @@ export default function Login() {
       </Head>
 
       <main>
-        <h1>로그인 페이지</h1>
-        <button type='button' onClick={onLogin}>
-          로그인 버튼
-        </button>
+        <Container
+          sx={{
+            height: '100vh',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <LoginForm />
+        </Container>
       </main>
     </div>
   );
