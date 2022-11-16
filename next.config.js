c5f58e0762de5c45b46c3eaa36fd69fb48647be1
taesung1993 <cheonyulin@gmail.com>
2022-11-16 15:20:58 +0900

    build: /dashboard로 접속하면 accounts로 리다이렉션 처리

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/dashboard/accounts',
+        permanent: true
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
