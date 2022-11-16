bb36dadbf0bd0514bf55da68c2e512a7b7b70186
taesung1993 <cheonyulin@gmail.com>
2022-11-16 23:32:18 +0900

    feat: 타입 추가

---
+++ utils/models/interfaces/IDAccount.ts
@@ -0,0 +1,12 @@
+export interface IDAccount {
+  id: number;
+  userName: string;
+  brokerName: string;
+  accountNumber: string;
+  accountStatus: string;
+  accountName: string;
+  assets: string;
+  payments: string;
+  isActive: string;
+  createdAt: string;
+}
