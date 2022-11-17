716eaae364311f19878113d2ec2007ff5d3fa01d
taesung1993 <cheonyulin@gmail.com>
2022-11-17 14:34:50 +0900

    feat: iuser에 password 옵션 추가, IUserSetting 타입 추가

--- utils/models/interfaces/IUser.ts
+++ utils/models/interfaces/IUser.ts
@@ -5,6 +5,7 @@ export interface IUser {
   name?: string;
   email: string;
   age?: number;
+  password?: string;
   gender_origin?: number;
   birth_date?: string;
   phone_number?: string;
---
+++ utils/models/interfaces/IUserSetting.ts
@@ -0,0 +1,10 @@
+export interface IUserSetting {
+  id: number;
+  uuid: string;
+  allow_marketing_push: boolean;
+  allow_invest_push: boolean;
+  is_active: boolean;
+  is_staff: boolean;
+  created_at: string;
+  updated_at: string;
+}
