e9de05ee8dcf8fd14be2eb026d6969a98f6e5022
taesung1993 <cheonyulin@gmail.com>
2022-11-17 16:59:43 +0900

    feat: 필요한 타입 추가

---
+++ utils/models/interfaces/IDUser.ts
@@ -0,0 +1,12 @@
+export interface IDUser {
+  accountCounts: number;
+  allowMarketingPush: boolean;
+  birthDate: string;
+  createdAt: string;
+  email: string;
+  genderOrigin: 4;
+  isActive: false;
+  lastLogin: string;
+  phoneNumber: string;
+  userName: string;
+}
